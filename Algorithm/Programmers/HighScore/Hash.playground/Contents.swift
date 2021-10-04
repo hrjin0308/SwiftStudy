f329218fad8d28173096344c4c80df923ab306a5
HRJin <wlsgpfla92@gmail.com>
2021-10-04 15:57:55 +0900

    [프로그래머스] 코딩테스트 > 해시 > 위장

---
+++ Algorithm/Programmers/HighScore/Hash.playground/Contents.swift
@@ -0,0 +1,4 @@
+
+print(Fake.solution([["yellowhat", "headgear"], ["bluesunglasses", "eyewear"], ["green_turban", "headgear"]]))
+print(Fake.solution([["crowmask", "face"], ["bluesunglasses", "face"], ["smoky_makeup", "face"]]))
+
