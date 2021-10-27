ab3c36d0ba7526440b5ef216803564ccdb79ffde
HRJin <wlsgpfla92@gmail.com>
2021-10-27 20:35:53 +0900

    SwiftUI로 HelloWorld 표시

---
+++ SwiftUI/01.HelloWorld/Shared/HelloWorld_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorld_SwiftUIApp.swift
+//  Shared
+//
+//  Created by 진혜림 on 2021/10/27.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorld_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
